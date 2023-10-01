51360aa6d204885971d5a7f708ec230db1425d13
Ryan Iguchi <ryan.iguchi1@gmail.com>
2023-10-01 13:45:50 +0200

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />s
     </Layout>
   );
 }
